7ef0bac79c1fd4fdefd6afdfe9c8e8d8201e191a
neska286 <nermen.yehia@hotmail.com>
2023-03-16 17:33:22 +0200

    first commit

---
+++ src/constants/paths.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    home: '/',
+    register:'/register',
+    login:'/login',
+    about:'/about',
+    postDetails:'/posts/:id',
+    pageNotFound: '*',
+   
+  };
